ae7dd943cbf24f65c81bc3f92781ba2daafbe87c
Thomas Gustafsson <thomas.gustafsson@usach.cl>
2025-06-05 23:20:16 -0400

    se logró gente (falta img del front)

--- rack-service/src/main/java/backend/rack/controller/RackController.java
+++ rack-service/src/main/java/backend/rack/controller/RackController.java
@@ -1,4 +1,16 @@
 package backend.rack.controller;
 
+import backend.rack.service.RackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rack")
 public class RackController {
+    @Autowired
+    RackService rackService;
+
+    // No tiene funciones específicas implementadas, ya que lo necesario para esta entrega lo puede hacer el frontend directamente
+
 }
--- rack-service/src/main/java/backend/rack/service/RackService.java
+++ rack-service/src/main/java/backend/rack/service/RackService.java
@@ -1,4 +1,10 @@
 package backend.rack.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class RackService {
+
+    // No tiene funciones específicas implementadas, ya que lo necesario para esta entrega lo puede hacer el frontend directamente
+
 }
